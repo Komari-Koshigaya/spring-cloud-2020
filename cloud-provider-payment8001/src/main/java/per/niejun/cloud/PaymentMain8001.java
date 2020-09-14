cf5802603c2208e21e3716788e7a1660f7f63a4b
niejun <2323384399@qq.com>
2020-09-14 10:31:35 +0800

    feat: 🎸 register module cloud-provider-payment8001 to eureka server

--- cloud-provider-payment8001/src/main/java/per/niejun/cloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/per/niejun/cloud/PaymentMain8001.java
@@ -1,8 +1,8 @@
 package per.niejun.cloud;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author NieJun
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2020/9/12 8:23
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
