bc40d1880f27f9a3d7fe952e20a775bad1bdc974
niejun <2323384399@qq.com>
2020-09-18 22:19:07 +0800

    feat: 🎸 register cloud-provider-payment8006 module to Consul

---
+++ cloud-providerconsul-payment8006/src/main/java/per/niejun/cloud/PaymentMain8006.java
@@ -0,0 +1,42 @@
+package per.niejun.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author NieJun
+ * @Description TODO
+ * @Date 2020/9/18 22:08
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+        System.out.println(
+                "               #########                       \n" +
+                "              ############                     \n" +
+                "              #############                    \n" +
+                "             ##  ###########                   \n" +
+                "            ###  ###### #####                  \n" +
+                "            ### #######   ####                 \n" +
+                "           ###  ########## ####                \n" +
+                "          ####  ########### ####               \n" +
+                "        #####   ###########  #####             \n" +
+                "       ######   ### ########   #####           \n" +
+                "       #####   ###   ########   ######         \n" +
+                "      ######   ###  ###########   ######       \n" +
+                "     ######   #### ##############  ######      \n" +
+                "    #######  ##################### #######     \n" +
+                "    #######  ##############################    \n" +
+                "   #######  ###### ################# #######   \n" +
+                "   #######  ###### ###### #########   ######   \n" +
+                "   #######    ##  ######   ######     ######   \n" +
+                "   #######        ######    #####     #####    \n" +
+                "    ######        #####     #####     ####     \n" +
+                "     #####        ####      #####     ###      \n" +
+                "      #####      ;###        ###      #        \n" +
+                "        ##       ####        ####              ");
+    }
+}
