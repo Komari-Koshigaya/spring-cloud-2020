a24d9f0051bcaeb595a520dd972af302014bdb42
niejun <2323384399@qq.com>
2020-09-19 22:14:57 +0800

    feat: 🎸 register cloud-consumerconsul-payment80 module to Consul

---
+++ cloud-consumerconsul-order80/src/main/java/per/niejun/cloud/OrderConsulMain80.java
@@ -0,0 +1,75 @@
+package per.niejun.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author NieJun
+ * @Description TODO
+ * @Date 2020/9/19 21:40
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class, args);
+        System.out.println(
+                "           ,                                                                \n" +
+                "          ,d$$,          ,;;                                                \n" +
+                "         J$$$$$,       :!!!>`>                                              \n" +
+                "         $$$$$$F     ,!!!!!';!                                              \n" +
+                "     ,!!,`$$$$$b    ,!!!:!';!!;                                             \n" +
+                "     <!``>`$$$$$b   ;!!'  ;!!!!                                             \n" +
+                "      `!>,_ \"$$$$b  >!  ,;!!!!`                                             \n" +
+                "         `<!;,,,,));((,<<!!>''                                              \n" +
+                "              ``'-)\\ccc,                                                    \n" +
+                "                 \"$c`$$$$bc,_                                               \n" +
+                "                  \"$$c\"?$$$$$$cc,._                                         \n" +
+                "                    \"$$,\\.?$$$$$$$$$cc,_                                    \n" +
+                "                      ??c\"?c`??$$$$$$$$$$bcc,_                              \n" +
+                "                        `?,_\"$cc,\"\"??$$$$$$$$$$$bcc,_                       \n" +
+                "                      .,,. `\"h,_\"?$$bc,_`\"??$$$$$$$$$$c,                    \n" +
+                "                 ,zd$$$$$$$L,  \"\"~=,.\"???$$$hhc,,,\"\"??$$$,                  \n" +
+                "             ,cc$$$$$$$$$$$$$b      `\"\"~=cc,\"\"???$$$$bc?$$$,                \n" +
+                "         ,zd$$$$$$$\"\",cccc\"?$$$,           ``\"\"$$ $$$$$,$$$$                \n" +
+                "   .,,d$$$$$$$$$$\",d$$\"\"$$$b,?$F\"              ?$>$$$$,$$$$F                \n" +
+                "  $$$$$$$$$$$$F,z$$\"      ?$$b,,,,JL,          ;$.$$P $$$$\"                 \n" +
+                "  $$$$$$$PF\",cdPP\"          ==\"\"\"\"\"\"????c,z, . ?$L\"\",$$$P'                  \n" +
+                "   \",,cccccPP\"                ,c=\"zcc$$$$$$$$$$cc,4$$$$\"                    \n" +
+                "                            z$$bc$$$$$$$$$$$$$$$$$ \"P\"                      \n" +
+                "                          ,P\",,\"$$$$$$$$$$$P\",,\"?$$r                        \n" +
+                "                         \\\",$$$L,?$$$$$$$P z$$$$b, /                        \n" +
+                "                        ,c:$$$$$$b'$$$$$F ???$$$$$ ,c                       \n" +
+                "                        d$:$$$\"    ?$$$$     \"$$$$:$$L                      \n" +
+                "                       4$$`$$$   + :$$$$   +  $$$F,$$$                      \n" +
+                "                 ,dMMMM,.F=`?$     :$$$$      $P\" ?777,MMMMMP=              \n" +
+                "                )MMMMMMMMMMhn.    ,J????,   ,nnnMMMMMMMMMMMMbn              \n" +
+                "              =\"3MMMMx\"T4MMMMM=?nF .:::.\")nM\"\"\"TTMMMMMMMMMMMMM\"             \n" +
+                "              dMMMMMMMMMMnnnnM. `b,`:::'xMM   ,dMbnnnMMMMMMMMMM             \n" +
+                "              4MMMMMMMMMMMMMMMMb.\"4MM MMMP   ,MMMMMMMMMMMMMMMMP             \n" +
+                "               `TTMMMMMMMMMMMMMMb.   =nM-   ,MMMMMMMMMMMMMMMPP              \n" +
+                "                      `\"\"\"\"TTMMMMb         xMMMMMMMPPP\"\"\"\"                  \n" +
+                "                               `\"Tn       dM\" \"                             \n" +
+                "                                ,,`b,   ,MP,,c?b,                           \n" +
+                "                             cd$$$h`TTMP\" d$$,c?$$                          \n" +
+                "                          ,c$$$$$$$$bc c$b`?$P44$P                          \n" +
+                "                          \"?$$$$$$$$P,c$$$$c,\"$F                            \n" +
+                "                           cbc$3$$$.z$$$$$$$$$ ?b,                          \n" +
+                "                            ?$P\" ,;'$$$$$$$$$$$`\"                           \n" +
+                "                           ,;-<!'>`!`?$$$$$$$$$F                            \n" +
+                "                         ,;!!!!:>! `<!`\"\"???\"\"\"                             \n" +
+                "                        '(``'!!)!!!, .`!!;;;;;                              \n" +
+                "                          ``- `'!!!;!,``;!!!!!;!,>                          \n" +
+                "                               -,'';!'!<<!'!`'''                            \n" +
+                "      ,xnnnnnnn,,_              `$c,``'`''``,                 ,,nnnnnnnx,   \n" +
+                "   ,xMP',xnndMMMMMMn  .           ?$$$$4$$$\"         ....=\",dMMnnbbbnx,\"\"M, \n" +
+                "  ,M\",dMMMMMPTTTMMMTMn \"?hcc,,,..  ?$$$ $$$,,,,ccc$$$$P\",nM,,,\"\"TTMMMMMMb,\"r\n" +
+                "  `M MMMMMP,uMMMMMMMMMMn ?$$$$$$$$$$$$$r4$$$$$$$$$$$$\"uMMMMMMMMMMn`TMMMMMM \\\n" +
+                "   \" MMMMP;MMMMMMMMMMMMML 3$$$$$$$PP\"\"_x=n,`\"\"\"\"??\"\",dMMMMMMMMMMMMM,4MMMMM  \n" +
+                "     MMMM>MMMMMMMMMMMMMMMbnnnnndMMMP\"'     `\"MMMMMMMMMMMMMMMMMMMMMMF MMMMP  \n" +
+                "      TMMM MMMMMMMMMMMMMMMMMMMMP\"               \"TMMMMMMMMMMMMMMMMM'JMMP'   \n" +
+                "        \"==`TMMMMMMMMMMMMMPP\"                      `TTMMMMMMMMMMP\"=~\"       \n" +
+                "              `\"\"\"\"\"\"\"\"                                 `\"\"\"\"\"              ");
+    }
+}
