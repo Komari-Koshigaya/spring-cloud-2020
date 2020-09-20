1cfd71e5d2dc1aacc1ae4b0489ec9352225c8c23
niejun <2323384399@qq.com>
2020-09-20 08:41:51 +0800

    feat: 🎸 ribbon along with Eureka, and load balance with getForEntity in restTemplate

--- cloud-consumer-order80/src/main/java/per/niejun/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/per/niejun/cloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package per.niejun.cloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import per.niejun.cloud.entities.CommonResult;
@@ -30,5 +31,16 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PRYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PRYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            log.info(entity.getStatusCode() + "\t" + entity.getHeaders());
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败");
+        }
     }
 }
