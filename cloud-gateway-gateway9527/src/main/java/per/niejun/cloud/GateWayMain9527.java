6d24421e3b78302c634f7c304ac70d810441601e
niejun <2323384399@qq.com>
2020-10-10 14:42:16 +0800

    feat: 🎸 completing GateWayMain9527 services and configuring Gateway route in application.yml

---
+++ cloud-gateway-gateway9527/src/main/java/per/niejun/cloud/GateWayMain9527.java
@@ -0,0 +1,30 @@
+package per.niejun.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author NieJun
+ * @Description TODO
+ * @Date 2020/10/10 14:25
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+        System.out.println(
+                "       :\\     /;               _ \n" +
+                "      ;  \\___/  ;             ; ;\n" +
+                "     ,:-\"'   `\"-:.            / ;\n" +
+                "_   /,---.   ,---.\\   _     _; / \n" +
+                "_:>((  |  ) (  |  ))<:_ ,-\"\"_,\"  \n" +
+                "    \\`````   `````/\"\"\"\"\",-\"\"     \n" +
+                "     '-.._ v _..-'      )        \n" +
+                "       / ___   ____,..  \\        \n" +
+                "      / /   | |   | ( \\. \\       \n" +
+                "ctr  / /    | |    | |  \\ \\      \n" +
+                "     `\"     `\"     `\"    `\"      ");
+    }
+}
