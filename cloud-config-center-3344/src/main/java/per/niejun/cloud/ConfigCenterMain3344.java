c87ff28ac9aa393a7fd4ffde9c108629824af316
niejun <2323384399@qq.com>
2020-10-12 20:38:29 +0800

    feat: 🎸 complete Config server configuration and test in ConfigCenterMain3344

---
+++ cloud-config-center-3344/src/main/java/per/niejun/cloud/ConfigCenterMain3344.java
@@ -0,0 +1,42 @@
+package per.niejun.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author NieJun
+ * @Description TODO
+ * @Date 2020/10/12 20:18
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+        System.out.println(
+                "  _________                        _____                              \n" +
+                " /    ___  \\                      /  _  \\                             \n" +
+                "|    /   )__)                    |  ( )__)                            \n" +
+                "|   |             _____   _____  |  |   ______  _____  __      _____  \n" +
+                "|   |     _____  /  _  \\ |  _  \\ |  |__(_    _)|   __)|  |    |     \\ \n" +
+                "|   |    (_   _)|  (_)  || (_) / |   __) |  |  |  (__ |  |    |  |\\  |\n" +
+                "|    \\____/  |  |       ||    <  |  |    |  |  |   __)|  |    |  | ) |\n" +
+                "|            |  |   _   ||     \\ |  |   _|  |_ |  (__ |  |___ |  |/  |\n" +
+                " \\__________/   |__( )__||__(\\__\\|__|  (______)|_____)|______)|_____/ \n" +
+                "   _____  __  __  ____                                                \n" +
+                "  |_   _|| (_) | |  __)                                               \n" +
+                "    | |  |  _  | |  __)                                               \n" +
+                "    |_|  |_( )_| |____)                     _   _                     \n" +
+                "  ________         ________              ``/_\\\"/_\\''                  \n" +
+                " /        \\       (__    __)             /-(o).(o)-\\                  \n" +
+                "|     __   |  _____  |  |                \\-\\_/-\\_/-/____,             \n" +
+                "|    |  )__) /  _  \\ |  |                 \\-      =  =  =\\            \n" +
+                "|    |   __ |  (_)  ||  |                 /-             -\\,          \n" +
+                "|    |__)  )|       ||  |                (=   |      /-    -\\__       \n" +
+                "|          ||   _   ||  |                 \\, _/-    _\\-   __|_-\\      \n" +
+                " \\________/ |__( )__||__|                (__(__/___(____/(_____/      "
+        );
+    }
+}
+
