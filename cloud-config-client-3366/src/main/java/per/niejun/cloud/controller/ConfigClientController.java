8a3c515a76be11fd39f3a19224383f64e4e5c8aa
niejun <2323384399@qq.com>
2020-10-14 09:38:00 +0800

    feat: 🎸 using SpringCloud Bus-message bus to dynamically refresh all ConfigClientMain like 3355、3366 by sending < curl -X POST "http://localhost:3344/actuator/bus-refresh" >

---
+++ cloud-config-client-3366/src/main/java/per/niejun/cloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package per.niejun.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author NieJun
+ * @Description TODO
+ * @Date 2020/10/13 21:17
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo: " + configInfo;
+    }
+
+}
