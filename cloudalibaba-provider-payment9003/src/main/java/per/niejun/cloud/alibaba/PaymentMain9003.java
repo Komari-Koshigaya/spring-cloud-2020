4ed43b8ece223b93e1dd40e80f10a209f50a5fbd
niejun <2323384399@qq.com>
2020-10-25 16:52:40 +0800

    feat: 🎸 complete payment side cluster in 9003 and 9004 before configuring sentinel service circuit-breaker

---
+++ cloudalibaba-provider-payment9003/src/main/java/per/niejun/cloud/alibaba/PaymentMain9003.java
@@ -0,0 +1,31 @@
+package per.niejun.cloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author NieJun
+ * @Description TODO
+ * @Date 2020/10/25 16:40
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9003 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9003.class, args);
+        System.out.println(
+                "    ____     \n" +
+                "   (__  '.   \n" +
+                "    /_____)  \n" +
+                "   ()@ @ ))) \n" +
+                "    'C ,()(()\n" +
+                "    ,.'_'.' \\\n" +
+                " __/ )   (--'\n" +
+                "'._./     \\  \n" +
+                "   (_._._._) \n" +
+                "    _|| ||_  \n" +
+                "mrf(__.).__) "
+        );
+    }
+}
